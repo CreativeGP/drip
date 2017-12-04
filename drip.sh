0149b6c79f3714d2fa229d36a1a415734f37b2cd
CreativeGP <malicktafaminecraft@gmail.com>
2017-12-04 22:20:40 +0900

    [Add] 変数が2個ではなかった場合'Missing Operand'と出力する

--- drip.sh
+++ drip.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-package_name=$1
-src=$2
-npm install ${package_name}
-browserify $2 > bundle.js
+if test $# -eq 2
+then
+  package_name=$1
+  src=$2
+  npm install ${package_name}
+  browserify $2 > bundle.js
+else
+  echo 'Missing Operand'
+fi
