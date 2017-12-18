db2f44bc02652f83f5c316de8a87e5f41cbc8064
CreativeGP <malicktafaminecraft@gmail.com>
2017-12-18 21:26:23 +0900

    [clean] インデントを整えた

--- drip.sh
+++ drip.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 if test $# -eq 2
 then
-  package_name=$1
-  src=$2
-  npm install ${package_name}
-  browserify $2 > bundle.js
+    package_name=$1
+    src=$2
+    npm install ${package_name}
+    browserify $2 > bundle.js
 else
-  echo 'Missing Operand'
+    echo 'Missing Operand'
 fi
