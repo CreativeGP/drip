47f8274cf62b4369a0c786a54c7940a7a4e239a7
CreativeGP <malicktafaminecraft@gmail.com>
2017-12-18 21:48:22 +0900

    [update] 引数関係を整理 副産物はデフォルトで削除するようにした

--- drip.sh
+++ drip.sh
@@ -1,10 +1,26 @@
 #!/bin/sh
-if test $# -eq 2
+if test $# -ge 2
 then
-    package_name=$1
-    src=$2
-    npm install ${package_name}
-    browserify $2 > bundle.js
+    # Parse arguments
+    while getopts i:n:s OPT
+    do
+	case $OPT in
+	    "s" ) FLG_SAVE_BYPRODUCTS="TRUE" ;;
+	    "i" ) FLG_PACKAGE_NAME="TRUE" ; PACKAGE_NAME="$OPTARG" ;;
+	    "n" ) FLG_SRC="TRUE" ; SRC="$OPTARG" ;;
+	esac
+    done
+
+    if [ ${FLG_PACKAGE_NAME} = "TRUE" ] && [ ${FLG_SRC} = "TRUE" ] ; then
+	# Install packages
+	npm install ${PACKAGE_NAME}
+	browserify ${SRC} > bundle.js
+	
+	# Delete byproducts (optional)
+	rm -r node_modules package_lock.json
+    else
+	echo 'drip -i input.js -n package-name -s(save byproducts)'
+    fi
 else
-    echo 'Missing Operand'
+    echo 'drip -i input.js -n package-name -s(save byproducts)'
 fi
