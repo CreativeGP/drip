ef5274d8ef64f80ec6c34f90ce1970da21ec2934
CreativeGP <malicktafaminecraft@gmail.com>
2018-04-09 11:38:29 +0900

    Add description for format of input.js

--- drip.sh
+++ drip.sh
@@ -4,23 +4,25 @@ then
     # Parse arguments
     while getopts i:n:s OPT
     do
-	case $OPT in
-	    "s" ) FLG_SAVE_BYPRODUCTS="TRUE" ;;
-	    "n" ) FLG_PACKAGE_NAME="TRUE" ; PACKAGE_NAME="$OPTARG" ;;
-	    "i" ) FLG_SRC="TRUE" ; SRC="$OPTARG" ;;
-	esac
+	      case $OPT in
+	          "s" ) FLG_SAVE_BYPRODUCTS="TRUE" ;;
+	          "n" ) FLG_PACKAGE_NAME="TRUE" ; PACKAGE_NAME="$OPTARG" ;;
+	          "i" ) FLG_SRC="TRUE" ; SRC="$OPTARG" ;;
+	      esac
     done
 
     if [ ${FLG_PACKAGE_NAME} = "TRUE" ] && [ ${FLG_SRC} = "TRUE" ] ; then
-	# Install packages
-	npm install ${PACKAGE_NAME}
-	browserify ${SRC} > bundle.js
-	
-	# Delete byproducts (optional)
-	rm -r node_modules package-lock.json
+	      # Install packages
+	      npm install ${PACKAGE_NAME}
+	      browserify ${SRC} > bundle.js
+
+	      # Delete byproducts (optional)
+	      rm -r node_modules package-lock.json
     else
-	echo 'drip -i input.js -n package-name -s(save byproducts)'
+	      echo 'drip -i input.js -n package-name -s(save byproducts)'
+        echo "input.js format: const ppp = require('ppp');"
     fi
 else
     echo 'drip -i input.js -n package-name -s(save byproducts)'
+    echo "input.js format: const ppp = require('ppp');"
 fi
